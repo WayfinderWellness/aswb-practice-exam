f53d86142568d6ea8b1fbf39d1d8cad566717710
WayfinderWellness <bryan@dawnperry.com>
2024-11-13 12:48:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -675,4 +675,5 @@ def handle_quiz_actions(next_clicks,
 
 # Run the app
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    #app.run_server(debug=True)
+    app.run_server(debug=True, host="0.0.0.0", port=8050)
